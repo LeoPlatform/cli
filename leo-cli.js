6222536246dca6e0d6e18d9e20cb504dbce2ec92
Steve <steve@roaringdevelopment.com>
2018-02-05 14:52:54 -0700

    allow for fixture overrides on events, fixtures/queue_dw.loadv2.json

--- leo-cli.js
+++ leo-cli.js
@@ -12,4 +12,5 @@ program
 	.command('run [directory]', "Run your lambda")
 	.command('create [type] [directory]', "Create a new leo system, bot, resource, or microservice")
 	.command('cron [id] [runner]', "Runs a cron handler for bot id")
+	.command('configure [leo_bus_stack] [dir]', "Download Runs a cron handler for bot id")
 	.parse(process.argv);
