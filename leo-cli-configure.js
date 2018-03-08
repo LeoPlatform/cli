0f1d022f80676d30772d52a89adcc10bcd5dc4bb
Joseph Lee Hunsaker <joseph.l.hunsaker@gmail.com>
2018-03-08 14:37:33 -0700

    clean up configure

--- leo-cli-configure.js
+++ leo-cli-configure.js
@@ -1,20 +1,13 @@
 #!/usr/bin/env node
 
-var path = require('path');
 var program = require('commander');
 var colors = require('colors');
-
-const utils = require("./lib/utils.js");
-
-const watch = require("node-watch");
-const fork = require("child_process").fork;
 
 const generateProfile = require("leo-sdk/lib/generateProfile.js");
 
 program
 	.version('0.0.1')
 	.option("-g, --global", "Install Globally")
-	.option("--region [region]", "Region to run cloudformation")
 	.usage('<stack> <region> <dir> [options]')
 	.action(function(stack, region, dir, options) {
 		if (typeof dir === "object") {
@@ -28,6 +21,7 @@ program
 			region: region
 		}, dir, (err) => {
 			console.log("done");
+			if (err) console.log(err);
 		});
 	})
 	.parse(process.argv);
