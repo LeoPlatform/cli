4c5051cfae9f884163fcdc6df0ac2a03d3e1493a
Steve <steve@roaringdevelopment.com>
2018-06-25 13:42:49 -0600

    allow for files to have directory names, allow for adding to an existing directory

--- leo-cli-create.js
+++ leo-cli-create.js
@@ -8,9 +8,9 @@ const utils = require('./lib/utils');
 
 program
 	.version('0.0.2')
-	.arguments('<type> <subtype> [dir] ')
-	.usage('<type> [subtype] <dir> [options]')
-	.action(async function(type, subtype, dir) {
+	.arguments('<type> <subtype> [dir] [name]')
+	.usage('<type> [subtype] <dir> [name] [options]')
+	.action(async function(type, subtype, dir, name) {
 
 		let pkgname = null;
 		let declaredType = type = type.toLowerCase();
@@ -71,54 +71,54 @@ program
 			fs.mkdirSync(prefix);
 		}
 
-		if (!fs.existsSync(prefix + dir)) {
-			let sUtils = Object.assign({
-				createLeoConfig: require("./lib/createLeoConfig.js"),
-				createLeoEnvironments: require('./lib/createLeoEnvironments.js'),
-				raw: function(value) {
-					if (typeof value === "object") {
-						return `_raw:${JSON.stringify(value)}`
-					}
-					return `_raw:${value}:raw_`;
-				},
-				storeFile: function(filename, template) {
-					let file = `'use strict';\nmodule.exports = ${JSON.stringify(template, null, 2)}`;
-					file = file.replace(/"_raw:(.*):raw_"/gm, "$1")
-						.replace(/\\n/g, "\n")
-						.replace(/\\t/g, "\t")
-						.replace(/^(\s*)"([^ -]*)":/gm, "$1$2:");
-					fs.writeFileSync(path.resolve(prefix + dir, filename), file);
-				},
-				npmInstall: function(cwd) {
-					if (!cwd) {
-						cwd = path.resolve(prefix + dir);
-					} else {
-						cwd = path.resolve(cwd);
-					}
-					console.log(`------ Running NPM Install on "${cwd}" ------`);
-					require('child_process').execSync("npm install", {
-						cwd: cwd
-					});
-				},
-				name: dir.replace(/[^a-zA-Z0-9]+/g, '_')
-			}, utils);
+		let sUtils = Object.assign({
+			createLeoConfig: require("./lib/createLeoConfig.js"),
+			createLeoEnvironments: require('./lib/createLeoEnvironments.js'),
+			raw: function(value) {
+				if (typeof value === "object") {
+					return `_raw:${JSON.stringify(value)}`
+				}
+				return `_raw:${value}:raw_`;
+			},
+			storeFile: function(filename, template) {
+				let file = `'use strict';\nmodule.exports = ${JSON.stringify(template, null, 2)}`;
+				file = file.replace(/"_raw:(.*):raw_"/gm, "$1")
+					.replace(/\\n/g, "\n")
+					.replace(/\\t/g, "\t")
+					.replace(/^(\s*)"([^ -]*)":/gm, "$1$2:");
+				fs.writeFileSync(path.resolve(prefix + dir, filename), file);
+			},
+			npmInstall: function(cwd) {
+				if (!cwd) {
+					cwd = path.resolve(prefix + dir);
+				} else {
+					cwd = path.resolve(cwd);
+				}
+				console.log(`------ Running NPM Install on "${cwd}" ------`);
+				require('child_process').execSync("npm install", {
+					cwd: cwd
+				});
+			},
+			name: dir.replace(/[^a-zA-Z0-9]+/g, '_')
+		}, utils);
 
-			let setupFile = path.resolve(__dirname, 'templates/', type, 'setup.js');
-			let setup = {
-				inquire: () => {},
-				process: () => {}
-			};
+		let setupFile = path.resolve(__dirname, 'templates/', type, 'setup.js');
+		let setup = {
+			inquire: () => {},
+			process: () => {}
+		};
 
-			if (fs.existsSync(setupFile)) {
-				setup = require(setupFile);
-			}
-			let setupContext = await setup.inquire(sUtils);
+		if (fs.existsSync(setupFile)) {
+			setup = require(setupFile);
+		}
+		let setupContext = await setup.inquire(sUtils);
 
-			switch (type) {
-				case 'quickstart':
-				case 'microservice':
-				case 'react':
-				case 'system':
+		switch (type) {
+			case 'quickstart':
+			case 'microservice':
+			case 'react':
+			case 'system':
+				if (!fs.existsSync(prefix + dir)) {
 					utils.copyDirectorySync(__dirname + "/templates/" + type, prefix + dir, {
 						'____DIRPATH____': parentName + "-" + dir.replace(/\s+/g, '_'),
 						'____DIRNAME____': dir.replace(/[^a-zA-Z0-9]+/g, '_')
@@ -126,16 +126,20 @@ program
 						/setup\.js$/,
 						new RegExp(`${__dirname}/templates/${type}.*node_modules`)
 					]);
-					break;
+				} else {
+					console.log("Directory already exists");
+					process.exit();
+				}
+				break;
 
-				case 'checksum':
-				case 'domainobject': // coming soon
-				case 'elasticsearch': // coming soon
-					if (parentType != "microservice" && parentType != "system") {
-						console.log(`Type ${type} must be within a system or microservice package`);
-						process.exit(1);
-					}
-
+			case 'checksum':
+			case 'domainobject': // coming soon
+			case 'elasticsearch': // coming soon
+				if (parentType != "microservice" && parentType != "system") {
+					console.log(`Type ${type} must be within a system or microservice package`);
+					process.exit(1);
+				}
+				if (!fs.existsSync(prefix + dir)) {
 					// setup variables needed to copy files
 					setupContext.type = type;
 					setupContext.prefix = prefix;
@@ -144,34 +148,44 @@ program
 					setupContext.declaredType = declaredType;
 
 					await setup.process(sUtils, setupContext);
-					break;
+				} else {
+					console.log("Directory already exists");
+					process.exit();
+				}
+				break;
 
-				default:
-					if (parentType != "microservice" && parentType != "system") {
-						console.log(`Type ${type} must be within a system or microservice package`);
-						process.exit(1);
+			default:
+				if (parentType != "microservice" && parentType != "system") {
+					console.log(`Type ${type} must be within a system or microservice package`);
+					process.exit(1);
+				}
+				templatePath = templatePath || `${__dirname}/templates/${type}`;
+				console.log(path.resolve(templatePath, "package.json"), fs.existsSync(path.resolve(templatePath, "package.json")));
+				if (!fs.existsSync(path.resolve(templatePath, "package.json")) || !fs.existsSync(prefix + dir)) {
+					if (!name) {
+						name = dir;
 					}
-					templatePath = templatePath || `${__dirname}/templates/${type}`;
 					utils.copyDirectorySync(templatePath, prefix + dir, {
-						'____DIRPATH____': parentName + "-" + dir.replace(/[^a-zA-Z0-9]+/g, '_'),
-						'____DIRNAME____': dir.replace(/[^a-zA-Z0-9]+/g, '_'),
-						'____BOTNAME____': parentName + "-" + dir.replace(/[^a-zA-Z0-9]+/g, '_'),
+						'____DIRPATH____': parentName + "-" + name.replace(/[^a-zA-Z0-9]+/g, '_'),
+						'____DIRNAME____': name.replace(/[^a-zA-Z0-9]+/g, '_'),
+						'____BOTNAME____': parentName + "-" + name.replace(/[^a-zA-Z0-9]+/g, '_'),
 						'____BOTTYPE____': declaredType
 					}, [
 						/setup\.js$/,
 						new RegExp(`${templatePath}.*node_modules`)
 					]);
-					break;
-			}
-			await setup.process(sUtils, setupContext);
-
-			sUtils.npmInstall();
-
-			console.log(`OK: Finished creating '${dir}'`);
-			process.exit();
-		} else {
-			console.log("Directory already exists");
+				} else {
+					console.log("Directory already exists");
+					process.exit();
+				}
+				break;
 		}
+		await setup.process(sUtils, setupContext);
+
+		sUtils.npmInstall();
+
+		console.log(`OK: Finished creating '${dir}'`);
+		process.exit();
 	})
 	.parse(process.argv);
 
