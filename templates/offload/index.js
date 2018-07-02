f18e7ec801dbcd7e5cf6418f0e9fb70e04ec1d71
Monty Charlton <monty@fullmonty.org>
2018-07-02 12:19:00 -0600

    Added wrapper to templates and allowed in-process execution (local debugging)

--- templates/offload/index.js
+++ templates/offload/index.js
@@ -1,18 +1,19 @@
 "use strict";
 const leo = require("leo-sdk");
 exports.handler = require("leo-sdk/wrappers/cron.js")(function (event, context, callback) {
-	let settings = Object.assign({}, event);
-	leo.offload({
-		id: event.botId,
-		queue: event.source,
-		each: (payload, meta, done) => {
-			console.log(payload);
-			console.log(meta);
-			done(null, true); // Report this event was handled
-		}
-	}, (err) => {
-		console.log("All done processing events", err);
-		callback(err);
-	});
-
+    let settings = Object.assign({
+        source: "____IN_QUEUE_NAME____"
+    }, event);
+    leo.offload({
+        id: context.botId,
+        queue: settings.source,
+        each: (payload, meta, done) => {
+            console.log(payload);
+            console.log(meta);
+            done(null, true); // Report this event was handled
+        }
+    }, (err) => {
+        console.log("All done processing events", err);
+        callback(err);
+    });
 });
