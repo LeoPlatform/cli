d93102637b02b0513c8769762df113fe5f2d0c29
Francis Lewis <flewis@leoinsights.com>
2018-07-30 17:06:57 -0600

    2.1.2 (#23)
    * Adding the number/string prompt back into the aggregations setup since numbers work.
    * Removed the prompt about aggregation id type.
    * Updated cloudformation to autoscale entity and aggregation tables and set their starting figures lower.

--- templates/aggregations/cloudformation/aggregations.js
+++ templates/aggregations/cloudformation/aggregations.js
@@ -1,206 +1,168 @@
-module.exports = {
-	"Resources": {
-		"LeoEventMapping": {
-			"Type": "AWS::Lambda::EventSourceMapping",
-			"Properties": {
-				"BatchSize": 500,
-				"Enabled": true,
-				"StartingPosition": "TRIM_HORIZON",
-				"EventSourceArn": {
-					"Fn::Sub": "${Entities.StreamArn}"
-				},
-				"FunctionName": {
-					"Fn::Sub": "${__bot02__}"
-				}
-			}
-		},
-		"LeoAggregationsMapping": {
-			"Type": "AWS::Lambda::EventSourceMapping",
-			"Properties": {
-				"BatchSize": 500,
-				"Enabled": true,
-				"StartingPosition": "TRIM_HORIZON",
-				"EventSourceArn": {
-					"Fn::Sub": "${Aggregations.StreamArn}"
-				},
-				"FunctionName": {
-					"Fn::Sub": "${__bot04__}"
-				}
-			}
-		},
-		"Entities": {
-			"Type": "AWS::DynamoDB::Table",
-			"Properties": {
-				"AttributeDefinitions": [
-					{
-						"AttributeName": "partition",
-						"AttributeType": "S"
-					},
-					{
-						"AttributeName": "id",
-						"AttributeType": "__entity_id_type__"
-					}
-				],
-				"KeySchema": [
-					{
-						"AttributeName": "partition",
-						"KeyType": "HASH"
-					},
-					{
-						"AttributeName": "id",
-						"KeyType": "RANGE"
-					}
-				],
-				"ProvisionedThroughput": {
-					"ReadCapacityUnits": 1000,
-					"WriteCapacityUnits": 2000
-				},
-				"StreamSpecification": {
-					"StreamViewType": "NEW_AND_OLD_IMAGES"
-				}
-			}
-		},
-		"Aggregations": {
-			"Type": "AWS::DynamoDB::Table",
-			"Properties": {
-				"AttributeDefinitions": [
-					{
-						"AttributeName": "id",
-						"AttributeType": "S",
-					},
-					{
-						"AttributeName": "bucket",
-						"AttributeType": "S"
-					}
-				],
-				"KeySchema": [
-					{
-						"AttributeName": "id",
-						"KeyType": "HASH"
-					},
-					{
-						"AttributeName": "bucket",
-						"KeyType": "RANGE"
-					}
-				],
-				"ProvisionedThroughput": {
-					"ReadCapacityUnits": 10,
-					"WriteCapacityUnits": 10
-				},
-				"StreamSpecification": {
-					"StreamViewType": "NEW_AND_OLD_IMAGES"
-				}
-			}
-		},
-		"LeoEntitiesChangesRole": {
-			"Type": "AWS::IAM::Role",
-			"Properties": {
-				"AssumeRolePolicyDocument": {
-					"Version": "2012-10-17",
-					"Statement": [
-						{
-							"Effect": "Allow",
-							"Principal": {
-								"Service": [
-									"lambda.amazonaws.com"
-								],
-								"AWS": {
-									"Fn::Sub": "arn:aws:iam::${AWS::AccountId}:root"
-								}
-							},
-							"Action": [
-								"sts:AssumeRole"
-							]
-						}
-					]
-				},
-				"ManagedPolicyArns": [
-					"arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
-					{
-						"Fn::ImportValue": {
-							"Fn::Sub": "${LeoBus}-Policy"
-						}
-					}
-				],
-				"Policies": [
-					{
-						"PolicyName": "Leo_Entities",
-						"PolicyDocument": {
-							"Version": "2012-10-17",
-							"Statement": [
-								{
-									"Effect": "Allow",
-									"Action": [
-										"dynamodb:Scan",
-										"dynamodb:PutItem",
-										"dynamodb:BatchWriteItem",
-										"dynamodb:BatchGetItem",
-										"dynamodb:UpdateItem",
-										"dynamodb:Query"
-									],
-									"Resource": [
-										{
-											"Fn::Sub": "${Entities.Arn}"
-										}
-									]
-								},
-								{
-									"Effect": "Allow",
-									"Action": [
-										"dynamodb:GetRecords",
-										"dynamodb:GetShardIterator",
-										"dynamodb:DescribeStream",
-										"dynamodb:ListStreams"
-									],
-									"Resource": [
-										{
-											"Fn::Sub": "${Entities.StreamArn}"
-										}
-									]
-								}
-							]
-						}
-					},
-					{
-						"PolicyName": "Leo_Aggregations",
-						"PolicyDocument": {
-							"Version": "2012-10-17",
-							"Statement": [
-								{
-									"Effect": "Allow",
-									"Action": [
-										"dynamodb:Scan",
-										"dynamodb:PutItem",
-										"dynamodb:BatchWriteItem",
-										"dynamodb:BatchGetItem",
-										"dynamodb:UpdateItem",
-										"dynamodb:Query"
-									],
-									"Resource": [
-										{
-											"Fn::Sub": "${Aggregations.Arn}"
-										}
-									]
-								},
-								{
-									"Effect": "Allow",
-									"Action": [
-										"dynamodb:GetRecords",
-										"dynamodb:GetShardIterator",
-										"dynamodb:DescribeStream",
-										"dynamodb:ListStreams"
-									],
-									"Resource": [
-										{
-											"Fn::Sub": "${Aggregations.StreamArn}"
-										}
-									]
-								}
-							]
-						}
-					}
-				]
+let cf = require("leo-aws/utils/cloudformation.js")();
+
+let aggregationsCF = cf.add({
+	"LeoEventMapping": {
+		"Type": "AWS::Lambda::EventSourceMapping",
+		"Properties": {
+			"BatchSize": 500,
+			"Enabled": true,
+			"StartingPosition": "TRIM_HORIZON",
+			"EventSourceArn": {
+				"Fn::Sub": "${Entities.StreamArn}"
+			},
+			"FunctionName": {
+				"Fn::Sub": "${__bot02__}"
 			}
 		}
 	}
-};
+})
+.add({
+	"LeoAggregationsMapping": {
+		"Type": "AWS::Lambda::EventSourceMapping",
+		"Properties": {
+			"BatchSize": 500,
+			"Enabled": true,
+			"StartingPosition": "TRIM_HORIZON",
+			"EventSourceArn": {
+				"Fn::Sub": "${Aggregations.StreamArn}"
+			},
+			"FunctionName": {
+				"Fn::Sub": "${__bot04__}"
+			}
+		}
+	}
+})
+.add({
+	"LeoEntitiesChangesRole": {
+		"Type": "AWS::IAM::Role",
+		"Properties": {
+			"AssumeRolePolicyDocument": {
+				"Version": "2012-10-17",
+				"Statement": [
+					{
+						"Effect": "Allow",
+						"Principal": {
+							"Service": [
+								"lambda.amazonaws.com"
+							],
+							"AWS": {
+								"Fn::Sub": "arn:aws:iam::${AWS::AccountId}:root"
+							}
+						},
+						"Action": [
+							"sts:AssumeRole"
+						]
+					}
+				]
+			},
+			"ManagedPolicyArns": [
+				"arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
+				{
+					"Fn::ImportValue": {
+						"Fn::Sub": "${LeoBus}-Policy"
+					}
+				}
+			],
+			"Policies": [
+				{
+					"PolicyName": "Leo_Entities",
+					"PolicyDocument": {
+						"Version": "2012-10-17",
+						"Statement": [
+							{
+								"Effect": "Allow",
+								"Action": [
+									"dynamodb:Scan",
+									"dynamodb:PutItem",
+									"dynamodb:BatchWriteItem",
+									"dynamodb:BatchGetItem",
+									"dynamodb:UpdateItem",
+									"dynamodb:Query"
+								],
+								"Resource": [
+									{
+										"Fn::Sub": "${Entities.Arn}"
+									}
+								]
+							},
+							{
+								"Effect": "Allow",
+								"Action": [
+									"dynamodb:GetRecords",
+									"dynamodb:GetShardIterator",
+									"dynamodb:DescribeStream",
+									"dynamodb:ListStreams"
+								],
+								"Resource": [
+									{
+										"Fn::Sub": "${Entities.StreamArn}"
+									}
+								]
+							}
+						]
+					}
+				},
+				{
+					"PolicyName": "Leo_Aggregations",
+					"PolicyDocument": {
+						"Version": "2012-10-17",
+						"Statement": [
+							{
+								"Effect": "Allow",
+								"Action": [
+									"dynamodb:Scan",
+									"dynamodb:PutItem",
+									"dynamodb:BatchWriteItem",
+									"dynamodb:BatchGetItem",
+									"dynamodb:UpdateItem",
+									"dynamodb:Query"
+								],
+								"Resource": [
+									{
+										"Fn::Sub": "${Aggregations.Arn}"
+									}
+								]
+							},
+							{
+								"Effect": "Allow",
+								"Action": [
+									"dynamodb:GetRecords",
+									"dynamodb:GetShardIterator",
+									"dynamodb:DescribeStream",
+									"dynamodb:ListStreams"
+								],
+								"Resource": [
+									{
+										"Fn::Sub": "${Aggregations.StreamArn}"
+									}
+								]
+							}
+						]
+					}
+				}
+			]
+		}
+	}
+})
+.add(cf.dynamodb.table("Entities", {
+	id: '__entity_id_type__',
+	partition: 'S',
+	autoscale: true,
+	throughput: {
+		read: 20,
+		write: 20
+	},
+	stream: "NEW_AND_OLD_IMAGES"
+}))
+.add(cf.dynamodb.table('Aggregations', {
+	id: 'S',
+	bucket: 'S',
+	autoscale: true,
+	throughput: {
+		read: 20,
+		write: 20
+	},
+	stream: "NEW_AND_OLD_IMAGES"
+}));
+
+module.exports = aggregationsCF;
