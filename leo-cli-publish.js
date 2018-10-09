913638c3dc2659407a042d2a150ba87f8ab8820c
Clint Zirker <czirker@leoinsights.com>
2018-10-09 11:43:52 -0600

    Added better error message when using -p and the stack doesn't exist

--- leo-cli-publish.js
+++ leo-cli-publish.js
@@ -97,7 +97,13 @@ const progressInterval = {
 			console.log(`Cannot determine base cloudformation from ${process.env.NODE_ENV}.  Cannot create patch.`);
 			process.exit();
 		}
-		startingCloudformation = await require("leo-aws")(target.leoaws).cloudformation.get(patch.stack, {});
+		try {
+			startingCloudformation = await require("leo-aws")(target.leoaws).cloudformation.get(patch.stack, {});
+		} catch (err) {
+			console.log(`Error getting base cloudformation from ${process.env.NODE_ENV}.  Cannot create patch.`);
+			console.log(err);
+			process.exit();
+		}
 	}
 
 	let data = await require("./lib/cloud-formation.js").createCloudFormation(rootDir, {
